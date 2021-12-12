7fbd7369cf634feb4eb9b9c87de7b2db40ba34f3
Mohammad AbuHijleh <mohammad.abuhijleh@iconnecths.com>
2021-12-12 16:13:45 +0200

    remove city

--- ExpoAdmin/Data/Migrations/20211212140550_remove.Designer.cs
+++ ExpoAdmin/Data/Migrations/20211212140550_remove.Designer.cs
@@ -4,14 +4,16 @@ using ExpoAdmin.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ExpoAdmin.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211212140550_remove")]
+    partial class remove
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
